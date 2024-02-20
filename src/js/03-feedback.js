d37d9fe9a1e10b26e7c67bb6e644ad177da734e1
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-20 21:35:43 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(() => {
+    const formData = {
+        email: emailInput.value.trim(),
+        message: messageInput.value.trim(),
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500));
