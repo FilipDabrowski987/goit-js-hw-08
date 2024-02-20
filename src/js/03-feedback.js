0bb9a6654c7cccf617cbc2b2a6a4cecc40fa696d
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-20 21:50:27 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,18 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
 form.addEventListener('input', throttle(() => {
-    const formData = {
+    const formLocalData = {
         email: emailInput.value.trim(),
         message: messageInput.value.trim(),
     };
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    localStorage.setItem('feedback-form-state', JSON.stringify(formLocalData));
 }, 500));
+
+window.addEventListener('load', => {
+    const storedLocalData = localStorage.getItem('feedback-form-state');
+    if(storedLocalData) {
+        const formLocalData = JSON.parse(storedData);
+        emailInput.value = formLocalData.email;
+        messageInput.value = formLocalData.message;
+    }
+});
