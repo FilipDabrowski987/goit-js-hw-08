cfc1e482b345b7c7ab8cec97205828fcc08757c2
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-27 20:05:13 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,20 @@
-import { Player } from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    player.on('timeupdate', throttle(() => {
+        player.getCurrentTime().then((time) => {
+        const timeString = JSON.stringify(time);
+        localStorage.setItem('videoplayer-current-time', timeString);
+  });
+    }, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
+    player.ready().then(() => {
+        const currentTime = localStorage.getItem('videoplayer-current-time');
+        if (currentTime !== null) {
+        const parsedTime = JSON.parse(currentTime);  
+        player.setCurrentTime(parsedTime);
+    }
     });
